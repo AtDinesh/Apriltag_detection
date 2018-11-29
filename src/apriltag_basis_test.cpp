609eab82ad2ab996eab1895cd69bf37cb7b18f41
AtDinesh <a.dinesh@hotmail.fr>
2018-11-29 11:14:23 +0100

    added src folder

---
+++ src/apriltag_basis_test.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <math.h>
+
+
+//#include "apriltag.h"
+//#include "tag36h11.h"
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
