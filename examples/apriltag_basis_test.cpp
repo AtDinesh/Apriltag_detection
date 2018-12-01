3c38ee8d3a47ecaeed902b438351104c3a223bd1
AtDinesh <a.dinesh@hotmail.fr>
2018-12-01 18:24:58 +0100

    [WIP] not working yet..

--- examples/apriltag_basis_test.cpp
+++ examples/apriltag_basis_test.cpp
@@ -5,11 +5,19 @@
 #include <unistd.h>
 #include <math.h>
 
-
 #include "apriltag.h"
-//#include "tag36h11.h"
+#include "tag36h11.h"
+#include "tag36h10.h"
+#include "tag25h9.h"
+#include "tag25h7.h"
+#include "common/zarray.h"
 
 int main(int argc, char *argv[])
 {
+    apriltag_family_t *tf = NULL;
+    tf = tag36h11_create();
+
+    //apriltag_detector_t *td = apriltag_detector_create();
+
     return 0;
 }
