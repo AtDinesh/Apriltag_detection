cb76150a1733183fad83c64af6a2b33f30188013
AtDinesh <a.dinesh@hotmail.fr>
2018-12-02 13:49:31 +0100

    load image

--- examples/apriltag_basis_test.cpp
+++ examples/apriltag_basis_test.cpp
@@ -60,6 +60,57 @@ int main(int argc, char *argv[])
                 printf("loading %s\n", path);
             else
                 printf("%20s ", path);
+
+            image_u8_t *im = NULL;
+            if (str_ends_with(path, "pnm") || str_ends_with(path, "PNM") ||
+                str_ends_with(path, "pgm") || str_ends_with(path, "PGM"))
+                im = image_u8_create_from_pnm(path);
+            else if (str_ends_with(path, "jpg") || str_ends_with(path, "JPG")) {
+                int err = 0;
+                pjpeg_t *pjpeg = pjpeg_create_from_file(path, 0, &err);
+                if (pjpeg == NULL) {
+                    printf("pjpeg error %d\n", err);
+                    continue;
+                }
+
+                if (1) {
+                    im = pjpeg_to_u8_baseline(pjpeg);
+                } else {
+                    printf("illumination invariant\n");
+
+                    image_u8x3_t *imc =  pjpeg_to_u8x3_baseline(pjpeg);
+
+                    im = image_u8_create(imc->width, imc->height);
+
+                    for (int y = 0; y < imc->height; y++) {
+                        for (int x = 0; x < imc->width; x++) {
+                            double r = imc->buf[y*imc->stride + 3*x + 0] / 255.0;
+                            double g = imc->buf[y*imc->stride + 3*x + 1] / 255.0;
+                            double b = imc->buf[y*imc->stride + 3*x + 2] / 255.0;
+
+                            double alpha = 0.42;
+                            double v = 0.5 + log(g) - alpha*log(b) - (1-alpha)*log(r);
+                            int iv = v * 255;
+                            if (iv < 0)
+                                iv = 0;
+                            if (iv > 255)
+                                iv = 255;
+
+                            im->buf[y*im->stride + x] = iv;
+                        }
+                    }
+                    image_u8x3_destroy(imc);
+                    if (td->debug)
+                        image_u8_write_pnm(im, "debug_invariant.pnm");
+                }
+
+                pjpeg_destroy(pjpeg);
+            }
+
+            /*if (im == NULL) {
+                printf("couldn't load %s\n", path);
+                continue;
+            }
         }
     }
 
